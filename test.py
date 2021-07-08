e4b4e4c7d6053c1105fb07e1bc8f77eb3bbf17fa
siddhant-shingi <siddhant@wadhwaniai.org>
2021-07-08 23:27:20 +0530

    updated test, train

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 import pickle
-from sklearn.datasets import make_regression
+from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
-import json
+import numpy as np
 
 model = pickle.load(open("models/model.pkl", "rb"))
 
 # Generate some data for validation
-X_test, y = make_regression(1000,n_features = 10)
+X, y = make_classification(1000,n_features = 10)
 
 # Test on the model
-y_hat = model.predict(X_test)
-
-print (accuracy_score(y, y_hat, normalize=False))
+print(model.score(X, y))
